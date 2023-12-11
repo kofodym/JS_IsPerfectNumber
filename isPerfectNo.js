9750b2a6bad1eb4b71540b684dc50a228dc5ae37
KofoCole <kofodym07@yahoo.com>
2023-12-11 09:50:12 +0100

    A function that calculates a perfect number

--- isPerfectNo.js
+++ isPerfectNo.js
@@ -4,10 +4,23 @@
 //  cond 2: Equivalently, a perfect number is a number that is half the sum of all of its positive divisors (including itself).
 
 // 1: Collect an input no from user
-// Initialize a variable to store the total of the correct positive divisors to be 0
-// Keep track of all divisors from 1 to number/2,
-// If number is sum of all divisors.
-
-function perfect_number(number) {}
+function perfect_number(number) {
+  // 2. Initialize a variable to store the total of the correct positive divisors to be 0
+  let sum = 0;
+  // Search all divisors from 1 to number/2
+  for (let i = 1; i <= number / 2; i++) {
+    if (number % i === 0) {
+      sum += i;
+    }
+  }
+  //   If number is sum of all divisors.
+  if (sum === number && sum !== 0) {
+    return `${number} is a perfect number`;
+  } else {
+    return`${number} is not a perfect number`;
+  }
+}
 
 console.log(perfect_number(20));
+console.log(perfect_number(144));
+console.log(perfect_number(28));
